a75f833afed713da69d8feeac1b57bfd02273577
kiranmys999 <kiran.dhananjaykd@gmail.com>
2014-10-28 10:59:25 +0000

    made a ton of changes which I can't remember lol

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,45 @@
 # Create your views here.
+from .models import Post
+## from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+'''
+def blog_list(request, *args, **kwargs):
+  post_list = Post.objects.filter(published=True)
+  template_name = "blog/post_list.html"
+  
+  context = {"post_list" : post_list}
+  
+  return render(request, template_name, context)
+'''
+
+'''
+def blog_detail(request, pk, *args, **kwargs):
+  post = Post.objects.get(pk=pk, published=True)
+  template_name = "blog/post_detail.html"
+  
+  context = {'post' : post}
+  
+  return render(request, template_name, context)
+'''
+
+class PublishedPostMixin(object):
+  def get_queryset(self):
+    queryset = super(PublishedPostMixin, self).get_queryset()
+    return queryset.filter(published=True)
+
+class PostListView(PublishedPostMixin, ListView):
+  model = Post
+  '''
+  def get_queryset(self):
+    queryset = super(PostListView, self).get_queryset()
+    return queryset.filter(published=True)
+  '''
+  
+class PostDetailView(PublishedPostMixin, DetailView):
+  model = Post
+  '''
+  def get_queryset(self):
+    queryset = super(PostDetailView, self).get_queryset()
+    return queryset.filter(published=True)
+  '''
--- microblog/urls.py
+++ microblog/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 from django.views.generic import TemplateView
+#from blog.views import blog_list, blog_detail
+from . import views
 
 admin.autodiscover()
 
@@ -15,6 +17,8 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    #url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^$', views.HomepageView.as_view(), name="home"),
+    url(r'^blog/', include("blog.urls", namespace="blog")),
     url(r'^admin/', include(admin.site.urls)),
 )
